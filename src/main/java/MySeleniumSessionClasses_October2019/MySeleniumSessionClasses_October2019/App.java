b9b5de6fda5a146e6ed351662909ca69da4ebc9a
kartikeybhardwaj25 <38663572+kartikeybhardwaj25@users.noreply.github.com>
2019-12-01 22:14:45 +0530

    first commit

---
+++ src/main/java/MySeleniumSessionClasses_October2019/MySeleniumSessionClasses_October2019/App.java
@@ -0,0 +1,13 @@
+package MySeleniumSessionClasses_October2019.MySeleniumSessionClasses_October2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
